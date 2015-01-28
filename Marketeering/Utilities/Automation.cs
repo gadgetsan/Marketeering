2df095e0af0261f51f0d150e8d7a36318d2bec56
Stephane.Beaudet <Stephane.Beaudet@csharricana.qc.ca>
2015-01-28 11:33:43 -0500

    Automation is now a Singleton, and it now uses AutoIt3

---
+++ Marketeering/Utilities/Automation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using AutoItX3Lib;
+
+namespace MouseKeyboardActivityMonitor.Demo.Classes
+{
+    class Automation
+    {
+        private static Automation instance;
+
+        private AutoItX3 autoIT = new AutoItX3();
+
+        public static Automation I
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Automation();
+                }
+                return instance;
+            }
+        }
+
+        public void mouseClick(Point location){
+
+            autoIT.MouseClick("primary", location.X, location.Y, 1);
+        }
+
+        public void rightMouseClick(Point location)
+        {
+
+            autoIT.MouseClick("secondary", location.X, location.Y, 1);
+        }
+
+        public void mouseMove(Point location){
+            autoIT.MouseMove(location.X, location.Y);
+        }
+    }
+}
