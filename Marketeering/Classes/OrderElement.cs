a3b1709297459fe797396efa4c95273b441c48cf
gadgetsan <stephaneblitz@gmail.com>
2015-02-15 15:41:23 -0500

    Ajout de la regle de 10% lors de l'achat

--- Marketeering/Classes/OrderElement.cs
+++ Marketeering/Classes/OrderElement.cs
@@ -23,13 +23,22 @@ namespace MouseKeyboardActivityMonitor.Demo.Classes
         {
             double proposedPrice = 0;
             //on va ordonner les données importés pour avoir seulement ceux de la station (Jita)
+            ObjectOtherOrders lowestSellPrice = latestExportedData.Where(i => i.jumps == 0 && i.bid == false).OrderBy(i => i.price).First();
+            ObjectOtherOrders highestBuyPrice = latestExportedData.Where(i => i.jumps == 0 && i.bid == true).OrderByDescending(i => i.price).First();
             if(isSellOrder){
-                ObjectOtherOrders lowestPrice = latestExportedData.Where(i => i.jumps == 0 && i.bid == false).OrderBy(i => i.price).First();
                 //on regarde la différence entre les deux prix pour ne pas se faire avoir
-                if (lowestPrice.price < orderAmmount)
+                if (lowestSellPrice.price < orderAmmount)
                 {
-                    proposedPrice = lowestPrice.price - 0.01;
+                    proposedPrice = highestBuyPrice.price - 0.01;
                 }
+
+
+                //si la différence est plus grande que 5%, on ne le fait pas
+                if (Math.Abs(orderAmmount - proposedPrice) / orderAmmount > 0.05)
+                {
+                    proposedPrice = 0;
+                }
+
             }else{
                 ObjectOtherOrders highestPrice = latestExportedData.Where(i => i.jumps == 0 && i.bid == true).OrderByDescending(i => i.price).First();
                 //on regarde la différence entre les deux prix pour ne pas se faire avoir
@@ -38,11 +47,12 @@ namespace MouseKeyboardActivityMonitor.Demo.Classes
                     //Notifier.I.Notify()
                     proposedPrice = highestPrice.price + 0.01;
                 }
-            }
-            //si la différence est plus grande que 5%, on ne le fait pas
-            if (Math.Abs(orderAmmount - proposedPrice) / orderAmmount > 0.05)
-            {
-                proposedPrice = 0;
+
+                //on regarde si le profit < 10%
+                if (((lowestSellPrice.price - highestBuyPrice.price) / highestBuyPrice.price) < 0.1)
+                {
+                    proposedPrice = 0;
+                }
             }
             return proposedPrice;
         }
